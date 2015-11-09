2496a027c405659592017ba7748547304702983b
Fridolin Pokorny <fpokorny@redhat.com>
2015-11-09 12:54:54 +0100

    Changer: implemented raw section printing

--- lib/SpecChanger.py
+++ lib/SpecChanger.py
@@ -34,12 +34,26 @@ class SpecChanger(SpecManipulator):
 	def getStatements(self):
 		return self.statements
 
+	def find_section(self, section_type):
+		for s in self.sections:
+			if type(s) is section_type:
+				return s
+
+		return None
+
+	def find_section_print(self, section_type, f = sys.stdout, verbose = True):
+		s = self.find_section(section_type)
+
+		if s is not None:
+			s.print_file(f)
+		elif verbose:
+			raise ValueError("Error: section '%s' not found" % section_type)
+
+		return s
+
 	def changelog_show(self, f = sys.stdout):
 		# TODO: do pretty print
-		for s in self.sections:
-			if type(s) is StChangelog:
-				s.print_file(f)
-				break
+		return self.find_section_print(StChangelog, f)
 
 	def changelog_add(self, items):
 		pass
@@ -48,28 +62,28 @@ class SpecChanger(SpecManipulator):
 		pass
 
 	def description_show(self, package = None, f = sys.stdout):
-		pass
+		return self.find_section_print(StDescription, f)
 
 	def description_edit(self, definition, package = None):
 		pass
 
 	def build_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StBuild, f)
 
 	def build_edit(self, build):
 		pass
 
 	def check_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StCheck, f)
 
 	def check_edit(self, edit):
 		pass
 
 	def clean_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StClean, f)
 
 	def files_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StFiles, f)
 
 	def files_add(self, files):
 		pass
@@ -78,13 +92,13 @@ class SpecChanger(SpecManipulator):
 		pass
 
 	def install_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StInstall, f)
 
 	def install_edit(self, install):
 		pass
 
 	def package_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StPackage, f)
 
 	def package_add(self, package):
 		pass
@@ -93,73 +107,73 @@ class SpecChanger(SpecManipulator):
 		pass
 
 	def prep_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StPrep, f)
 
 	def prep_edit(self, prep):
 		pass
 
 	def pre_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StPre, f)
 
 	def pre_edit(self, pre):
 		pass
 
 	def post_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StPost, f)
 
 	def post_edit(self, post):
 		pass
 
 	def preun_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StPreun, f)
 
 	def preun_edit(self, preun):
 		pass
 
 	def postun_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StPostun, f)
 
 	def postun_edit(self, postun):
 		pass
 
 	def pretrans_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StPretrans, f)
 
 	def pretrans_edit(self, pretrans):
 		pass
 
 	def posttrans_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StPosttrans, f)
 
 	def posttrans_edit(self, posttrans):
 		pass
 
 	def triggerin_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StTriggerin, f)
 
 	def triggerin_edit(self, triggerin):
 		pass
 
 	def triggerprein_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StTriggerprein, f)
 
 	def triggerprein_edit(self, triggerprein):
 		pass
 
 	def triggerun_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StTriggerun, f)
 
 	def triggerun_edit(self, triggerun):
 		pass
 
 	def triggerpostun_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StPostun, f)
 
 	def triggerpostun_edit(self, triggerpostun):
 		pass
 
 	def verifyscript_show(self, f = sys.stdout):
-		pass
+		return self.find_section_print(StVerifyscript, f)
 
 	def verifyscript_edit(self, verifyscript):
 		pass
